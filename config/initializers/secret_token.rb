4c133fd0a4286e7867fea4476894c07a2c3c798f
dlamichhane <dlamichhane@hotmail.com>
2013-06-30 21:34:24 +0300

    Initialiation of the work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialpost::Application.config.secret_key_base = '8bd726e4c06fb3740837daaac127204724f3ad4aeb4bf7c6ad61a55f2803ed69194f6a4e2f482e3592c4d3d5d684a22294da99a67802bc4eee2ae989f658ca98'
